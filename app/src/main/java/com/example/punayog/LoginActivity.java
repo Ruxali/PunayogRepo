4a3f72f26e52ce02bea74e9f56ab4009e67ca7f0
Aarati Adhikari <adhikariaarati68@gmail.com>
2022-04-07 09:21:14 +0545

    dberror

--- app/src/main/java/com/example/punayog/LoginActivity.java
+++ app/src/main/java/com/example/punayog/LoginActivity.java
@@ -96,7 +96,6 @@ public class LoginActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if (task.isSuccessful()) {
                     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                    assert user != null;
                     if (user.isEmailVerified()) {
                         onLoginButtonClick();
                     } else {
