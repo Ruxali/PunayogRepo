54b7687dbe679d1671945d96f3c5c20a6f2338e7
Aarati Adhikari <adhikariaarati68@gmail.com>
2022-04-09 20:30:52 +0545

    herta

--- app/src/main/java/com/example/punayog/AddProductActivity.java
+++ app/src/main/java/com/example/punayog/AddProductActivity.java
@@ -2,19 +2,150 @@ package com.example.punayog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class AddProductActivity extends AppCompatActivity {
+    private EditText editTextProductName, editTextPrice, editTextShortText, editTextLongDesc, editTextLocation;
+    private Spinner spinnerCategory, spinnerSubCategory;
+    private Button postButton;
+    private FirebaseDatabase firebaseDatabase;
+    private String record;
+    private String names[] = {"Accessories", "Apparels", "Books", "Electronics"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_product);
         statusBarColor();
+        editTextProductName = findViewById(R.id.editTextProductName);
+        editTextPrice = findViewById(R.id.editTextPrice);
+        editTextLongDesc = findViewById(R.id.editTextLongDesc);
+        editTextShortText=findViewById(R.id.editTextShortDesc);
+        editTextLocation = findViewById(R.id.editTextLocation);
+        spinnerCategory = findViewById(R.id.spinnerCategory);
+        spinnerSubCategory = findViewById(R.id.spinnerSubCategory);
+        postButton = findViewById(R.id.postButton);
+        mAuth = FirebaseAuth.getInstance();
+        postButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validateProductName() || !validatePrice() || !validateLong() || !validateLocation() || !validateShort()) {
+                    return;
+                } else {
+                    onPostClick();
+                }
+
+            }
+        });
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, names);
+        spinnerCategory.setAdapter(adapter);
+
+
+        spinnerCategory.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        record = "Accessories";
+                        Toast.makeText(AddProductActivity.this, "Accesories", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        record = "Apparels";
+                        Toast.makeText(AddProductActivity.this, "Apparels", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        record = "Books";
+                        Toast.makeText(AddProductActivity.this, "Books", Toast.LENGTH_SHORT).show();
+                        break;
+                    case 3:
+                        record = "Electronics";
+                        Toast.makeText(AddProductActivity.this, "Electronics", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + i);
+                }
+
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+                Toast.makeText(AddProductActivity.this, "Please select the category", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
+    private boolean validateProductName() {
+        String inputProductName = editTextProductName.getText().toString().trim();
+        if (inputProductName.isEmpty()) {
+            Toast.makeText(this, "Product name is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!inputProductName.matches("^[a-zA-Z0-9]+([ ]?[a-zA-Z0-9]+)*$")) {
+            Toast.makeText(this, "Name pattern is not matched", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    private boolean validatePrice() {
+        String inputPrice = editTextPrice.getText().toString().trim();
+        if (inputPrice.isEmpty()) {
+            Toast.makeText(this, "Please enter the price", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateShort() {
+        String inputShort = editTextShortText.getText().toString().trim();
+        if (inputShort.isEmpty()) {
+            Toast.makeText(this, "Please provide the short description", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateLong() {
+        String inputLongDesc = editTextLongDesc.getText().toString().trim();
+        if (inputLongDesc.isEmpty()) {
+            Toast.makeText(this, "Please provide the description", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private boolean validateLocation() {
+        String addInput = editTextLocation.getText().toString().trim();
+        if (addInput.isEmpty()) {
+            Toast.makeText(this, "Address is required", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 
     public void statusBarColor() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -24,8 +155,12 @@ public class AddProductActivity extends AppCompatActivity {
         }
     }
 
-    public void onBackClick(View view) {
-        startActivity(new Intent(this, MainActivity.class));
-        overridePendingTransition(R.anim.slide_in_left, R.anim.stay);
+    public void onPostClick(View view) {
+
+
+    }
+
+    private void onPostClick() {
+        startActivity(new Intent(AddProductActivity.this, MainActivity.class));
     }
 }
--- app/src/main/java/com/example/punayog/MainActivity.java
+++ app/src/main/java/com/example/punayog/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -19,7 +21,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
 
@@ -49,32 +51,34 @@ public class MainActivity extends AppCompatActivity {
         statusBarColor();
 
         //side navigation
+
         drawerLayout = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.sideNavView);
+        navigationView.bringToFront();
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
         //fragments
-        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container,homeFragment).commit();
+        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container, homeFragment).commit();
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()){
+                switch (item.getItemId()) {
                     case R.id.homeButton:
-                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container,homeFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container, homeFragment).commit();
                         return true;
 
                     case R.id.profile:
-                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container,profileFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container, profileFragment).commit();
                         return true;
 
                     case R.id.chat:
-                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container,chatFragment).commit();
+                        getSupportFragmentManager().beginTransaction().replace(androidx.navigation.ui.R.id.container, chatFragment).commit();
                         return true;
 
                     case R.id.cart:
@@ -85,12 +89,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        navigationView.setNavigationItemSelectedListener(this);
     }
-    public void statusBarColor(){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            getWindow().setStatusBarColor(getResources().getColor(R.color.themeColor2,this.getTheme()));
-        }else if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+
+
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public void statusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.themeColor2, this.getTheme()));
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().setStatusBarColor(getResources().getColor(R.color.themeColor2));
         }
     }
@@ -103,9 +117,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.navigation_menu,menu);
+        menuInflater.inflate(R.menu.navigation_menu, menu);
+        menuInflater.inflate(R.menu.side_navigation, menu);
         return true;
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 }
